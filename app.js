92bcd34f3b06330439046301291c9c081484196d
Nico <nicolaslorthios@hotmail.com>
2017-04-28 08:16:21 +0200

    Reformat tableau en forme

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.set('messages', {});
-app.set('modeBot', true);
+app.set('modeBot', false);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/webhook.js
+++ routes/webhook.js
@@ -89,6 +89,15 @@ router.get('/admin', function(req, res, next) {
 });
 
 
+/* Switch bot mode */
+router.get('/switchMode', function(req, res, next) {
+  var modeBot = req.app.get('modeBot');
+  console.log('modeBot was '+modeBot);
+  var newMode = !modeBot;
+  req.app.set('modeBot', newMode);
+  console.log('modeBot now is '+newMode);
+  res.status(200).json(newMode);
+})
 
 
 
