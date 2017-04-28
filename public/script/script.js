c817b4544d05564a10d41c7673019f6d91d97b6a
Nico <nicolaslorthios@hotmail.com>
2017-04-28 09:53:50 +0200

    Route localhost

--- public/script/script.js
+++ public/script/script.js
@@ -39,7 +39,7 @@ function switchMode () {
                  cache: 'default' };
 
   // fetch('https://one-c3po.herokuapp.com/switchMode', myInit)
-  fetch('localhost:3000/switchMode', myInit)
+  fetch('http://localhost:3000/switchMode', myInit)
   .then(function(response) {
     console.log(response);
   })
